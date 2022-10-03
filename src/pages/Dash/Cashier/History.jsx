a4430e3e9066284546adaf6fbf7652641fd57700
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2022-10-03 15:41:43 +0100

    crash fix on history page

--- src/pages/Dash/Cashier/History.jsx
+++ src/pages/Dash/Cashier/History.jsx
@@ -8,7 +8,7 @@ const History = () => {
   useEffect(() => {
     const getTransactions = async () => {
       const allTransactions = await GetTransactions();
-      setTransactions(allTransactions?.data);
+      setTransactions(allTransactions?.data | []);
     };
     getTransactions();
   }, []);
@@ -17,7 +17,7 @@ const History = () => {
     <>
       <div className="historyBlockContainer">
         <div className="historyBlocks">
-          {transactions.length > 0 ? (
+          {transactions?.length > 0 ? (
             transactions?.map((transaction) => (
               <HistoryBlock
                 amount={transaction.amount}
