5b1e61bd3de5d61f284d6e32d0e8a7a40005bd2f
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2022-10-04 21:23:55 +0100

    Implement lazy loading

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-
-import SignIn from "./pages/Auth/SignIn";
-import SignUp from "./pages/Auth/SignUp";
-import Home from "./pages/Main/Home";
-import NotFound from "./pages/Main/NotFound";
-import Trading from "./pages/Dash/Trading";
-import Profile from "./pages/Dash/Profile";
-import Cashier from "./pages/Dash/Cashier";
-import Deposit from "./pages/Dash/Cashier/Deposit";
-import MainDash from "./pages/Dash/MainDash";
-import Withdraw from "./pages/Dash/Cashier/Withdraw";
-import Status from "./pages/Dash/Status";
-import History from "./pages/Dash/Cashier/History";
 
 // import "./assets/themes/light_mode.css";
 import "./assets/themes/dark_mode.css";
 import ScrollToTop from "./components/ScrollToTop";
+
+const Home = lazy(() => import("./pages/Main/Home"));
+const NotFound = lazy(() => import("./pages/Main/NotFound"));
+const SignIn = lazy(() => import("./pages/Auth/SignIn"));
+const SignUp = lazy(() => import("./pages/Auth/SignUp"));
+const Trading = lazy(() => import("./pages/Dash/Trading"));
+const Profile = lazy(() => import("./pages/Dash/Profile"));
+const MainDash = lazy(() => import("./pages/Dash/MainDash"));
+const Cashier = lazy(() => import("./pages/Dash/Cashier"));
+const Status = lazy(() => import("./pages/Dash/Status"));
+const Deposit = lazy(() => import("./pages/Dash/Cashier/Deposit"));
+const Withdraw = lazy(() => import("./pages/Dash/Cashier/Withdraw"));
+const History = lazy(() => import("./pages/Dash/Cashier/History"));
 
 const App = () => {
   return (
@@ -36,33 +37,44 @@ const App = () => {
 
       <BrowserRouter>
         <ScrollToTop>
-          <Routes>
-            <Route path="/" element={<Home />} />
+          <Suspense
+            fallback={
+              <div className="loadingState">
+                <img
+                  src="/public_assets/images/Thrida-01-02.png"
+                  alt="Thrida"
+                />
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Home />} />
 
-            {/* Auth Pages */}
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
+              {/* Auth Pages */}
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
 
-            {/* Dashboard Pages */}
-            <Route path="/dashboard" element={<MainDash />}>
-              <Route path="" element={<Navigate to={"trading"} />} />
-              <Route path="trading" element={<Trading />} />
-              <Route path="myaccount" element={<Profile />} />
+              {/* Dashboard Pages */}
+              <Route path="/dashboard" element={<MainDash />}>
+                <Route path="" element={<Navigate to={"trading"} />} />
+                <Route path="trading" element={<Trading />} />
+                <Route path="myaccount" element={<Profile />} />
 
-              {/* Cashier And Transaction Routes */}
-              <Route path="cashier" element={<Cashier />}>
-                <Route path="" element={<Navigate to={"deposit"} />} />
-                <Route path="deposit" element={<Deposit />} />
-                <Route path="withdraw" element={<Withdraw />} />
-                <Route path="history" element={<History />} />
+                {/* Cashier And Transaction Routes */}
+                <Route path="cashier" element={<Cashier />}>
+                  <Route path="" element={<Navigate to={"deposit"} />} />
+                  <Route path="deposit" element={<Deposit />} />
+                  <Route path="withdraw" element={<Withdraw />} />
+                  <Route path="history" element={<History />} />
+                </Route>
+
+                <Route path="status" element={<Status />} />
               </Route>
 
-              <Route path="status" element={<Status />} />
-            </Route>
-
-            {/* Utility Pages */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+              {/* Utility Pages */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </ScrollToTop>
       </BrowserRouter>
     </>
